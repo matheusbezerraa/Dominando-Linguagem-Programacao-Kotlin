450ff5284fa468929ec5df7c38a63308b61809f2
Matheus Freire Bezerra <matheus.freire.bezerra@gmail.com>
2023-05-19 18:01:03 -0300

    COMMIT map e funções uteis

--- Estruturas-Controle-Fluxo-e-Colecoes-Kotlin/Controle de Fluxo/Iterator/Iterator.kt
+++ Estruturas-Controle-Fluxo-e-Colecoes-Kotlin/Controle de Fluxo/Iterator/Iterator.kt
@@ -2,8 +2,8 @@ class Animal(val name: String)
 
 class Zoo(val animals: List<Animal>) {
 
-    operator fun iterator(): Iterator<Animal> {             // 1
-        return animals.iterator()                           // 2
+    operator fun iterator(): Iterator<Animal> {             
+        return animals.iterator()                           
     }
 }
 
@@ -11,7 +11,7 @@ fun main() {
 
     val zoo = Zoo(listOf(Animal("zebra"), Animal("lion"), Animal("monkey")))
 
-    for (animal in zoo) {                                   // 3
+    for (animal in zoo) {                                   
         println("Watch out, it's a ${animal.name}")
     }
 
